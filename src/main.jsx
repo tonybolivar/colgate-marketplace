308f9cb89536ea70cfa62d7a81433e6b0995836b
tonybolivar <tony.e.bolivar@gmail.com>
2026-02-25 20:45:15 -0500

    Install and configure Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from '@/contexts/AuthContext'
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
         <App />
       </AuthProvider>
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 )
